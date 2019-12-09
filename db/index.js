8ac4b38b4dc15e170b8ef62ef69df59795d7ba7a
Andrew O'Grady <andrewogrady3@gmail.com>
2019-12-09 18:50:37 -0500

    added db schema

--- db/index.js
+++ db/index.js
@@ -5,6 +5,20 @@ const Item = require("./models/Item")
 const Keyword = require("./models/Keyword")
 const Pieces = require("./models/Pieces")
 
+
+//One to One relationship
+User.belongsTo(Bucket)
+Bucket.hasOne(User)
+
+//Many to Many relationships!
+Item.belongsToMany(Bucket, {through: 'Bucket_Item'})
+
+Item.belongsToMany(Pieces, {through: 'Item_Pieces'})
+
+Item.belongsToMany(Keyword, {through: 'Item_Keyword'})
+
+
+
 module.exports = {
     db,
     User,
