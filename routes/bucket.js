eb1ea7d19144b7fcfc3864a592778c4ae338934e
Andrew O'Grady <andrewogrady3@gmail.com>
2019-12-10 22:38:59 -0500

    added api routes

--- routes/bucket.js
+++ routes/bucket.js
@@ -1,8 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { Item, User, Bucket } = require('../db')
 
-router.get('/', function(req, res, next) {
-    res.send('TESTING BUCKET');
-  });
+//Get all Books based on UserID
+/**
+ * This API route when hit will respond with all the information of a certain block in a person's cart
+ */
+  router.get('/:id', async (req, res, next) => {
+
+      try {
+          const item = await Item.findOne({
+              include: [{
+                model: Bucket,
+                include: {model: User}
+              }],
+              where: {
+                  id: req.params.id
+              }
+          })
+          res.json(item)
+      }catch(err) {
+          next(err)
+      }
+  })
 
 module.exports = router
